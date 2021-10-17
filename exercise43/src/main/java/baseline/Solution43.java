7008e96b820eb84a116842d84c3abfd8695791f6
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-17 17:56:34 -0400

    All exercise folders added

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -7,35 +7,95 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Solution43 {
-
-
+    //This equation tests continuously to see if the user put in y or n and is called by input collector
     public static boolean yesOrno(){
-        //This equation tests continuously to see if the user put in y or n
-        // and is called by input collector
-        //It will be used by other equations instead of duplicating code
-        return false;
+        boolean result;
+        Scanner input = new Scanner(System.in);
+        do {
+            String choice = input.next();
+            if (choice.equals("y")) {
+                result = true;
+                break;
+            } else if (choice.equals("n")) {
+                result = false;
+                break;
+            } else {
+                System.out.println("Wrong Input please enter y or n !");
+            }
+        } while (true);
+
+        return result;
+
     }
     //Function collecting input and storing it in website class
     static Website collectInput()
     {
-        //Will collect initial user input and construct website off it as it gathers it
-        //will call equation to see if user put y or n
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Site name:");
+        String websiteName = input.next();
+        System.out.println("Author: ");
+        String websiteAuthor = input.next();
 
-        return null;
+        System.out.println("Do you want a folder for JavaScript?");
+        //Calls equation to see if user put y or n
+        boolean javScripFolder = Solution43.yesOrno();
+        System.out.println("Do you want a folder for CSS?");
+        boolean cssFolder = Solution43.yesOrno();
+
+        return new Website(websiteName,websiteAuthor,javScripFolder,cssFolder);
+
     }
-
+    //creator that will create website and test for its successful creation
     public static void creator(Website websiteInfo){
-        //creator that will create website and test for its successful creation
-    }
+        //creates folder using info on current directory
+        File newFolder = new File(websiteInfo.currentPath);
+        boolean websiteTest = newFolder.mkdir();
 
+        if(websiteTest){
+            System.out.println("Created ./website/"+ websiteInfo.Name);
+            htmlFile(websiteInfo);
+            createCSSJava(websiteInfo);
+        }else{
+            System.out.println("Could not create website");
+        }
+    }
+    //Will take care of creating the html_File
     public static void htmlFile(Website websiteInfo){
-        //Will take care of creating the html_File
+        File file = new File(websiteInfo.currentPath + "/index.html");
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+            writer.write(websiteInfo.htmltext);
+            System.out.println(websiteInfo.SuccessMessage + websiteInfo.Name + "/index.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
     //Depending on User input creates CSS and/or/neither Java folders
     public static void createCSSJava(Website websiteInfo){
-         //Will prompt and create css if told to
+        if(websiteInfo.JavFol)
+        {
+            File newFolder = new File(websiteInfo.currentPath + "/js");
+            boolean folderTest = newFolder.mkdir();
 
+            if (folderTest) {
+                System.out.println(websiteInfo.SuccessMessage + websiteInfo.Name + "/js");
+            } else {
+                System.out.println("Could not create folder");
+            }
+        }
+        if(websiteInfo.CssFol)
+        {
+            File newFolder = new File(websiteInfo.currentPath + "/CSS");
+            boolean folderTest = newFolder.mkdir();
+
+            if (folderTest) {
+                System.out.println("Created ./website/" + websiteInfo.Name + "/CSS");
+            } else {
+                System.out.println("Could not create folder");
+            }
+        }
     }
 
     //main that will call functions and pass in The variables to fill with input and create files with
--- exercise43/src/main/java/baseline/Website.java
+++ exercise43/src/main/java/baseline/Website.java
@@ -3,11 +3,22 @@ package baseline;
 //class website to sore all website info and pass between functions
 public class Website {
     //Attributes of website class*Q
-
+    String Name;
+    String Author;
+    boolean JavFol;
+    boolean CssFol;
+    String currentPath;
+    String htmltext;
+    String SuccessMessage = "Created ./website/";
 
     Website(String WebsiteName,String WebsiteAuthor, boolean JavScrip_Folder, boolean CSS_Folder)
     {
-        //Website Contrsuctor
+        JavFol = JavScrip_Folder;
+        CssFol = CSS_Folder;
+        Name = WebsiteName;
+        Author = WebsiteAuthor;
+        currentPath = "src/main/java/baseline/" + WebsiteName;
+        htmltext = "<title> " + WebsiteName + " </title>\n" +
+                "<meta> " + WebsiteAuthor + " </meta>";
     }
-
 }
